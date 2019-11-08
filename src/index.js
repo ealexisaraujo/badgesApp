e2a85db96232f8d5bd6acc351ad996ab301be487
Alexis Araujo <ala1289@gmail.com>
2019-11-07 20:13:35 -0600

    ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi badges!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+const element = <h1>Hello Platzi</h1>;
+const container = document.getElementById('app');
+
+// ReactDOM.render(__que__,__donde___)
+ReactDOM.render(element, container);
